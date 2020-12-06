b6da1f59aefdfd1d7cfe21369c216b894d2b9f21
guivictorr <guilhermevictor20@gmail.com>
2020-12-06 00:40:02 -0300

    feat(cart): created cart page footer

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -2,7 +2,17 @@ import React, { useContext } from 'react';
 import { ProductsContext } from '../../context/productsContext';
 import { FlatList } from 'react-native';
 
-import { CartContainer, CartHeader } from './styles';
+import {
+  CartContainer,
+  CartHeader,
+  CartTotalValue,
+  CartSubTotalValue,
+  CartShipValue,
+  CartFooter,
+  CartCheckOutButton,
+  CartCheckOutButtonText,
+  CartFooterValues,
+} from './styles';
 
 import CartItem from '../../components/CartItem';
 import RoundedButton from '../../components/RoundedButton';
@@ -21,6 +31,16 @@ const Cart = ({ navigation }) => {
         renderItem={({ item }) => <CartItem title={item.name} />}
         style={{ width: '100%', marginTop: 20 }}
       />
+      <CartFooter>
+        <CartCheckOutButton>
+          <CartCheckOutButtonText>Finalizar</CartCheckOutButtonText>
+        </CartCheckOutButton>
+        <CartFooterValues>
+          <CartTotalValue>Total: R$200,00</CartTotalValue>
+          <CartSubTotalValue>Subtotal: R$100,00</CartSubTotalValue>
+          <CartShipValue>Frete: R$100,00</CartShipValue>
+        </CartFooterValues>
+      </CartFooter>
     </CartContainer>
   );
 };
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -17,3 +17,42 @@ export const CartHeader = styled.View`
   margin-top: 35px;
   padding: 0 25px;
 `;
+
+export const CartFooter = styled.View`
+  height: 100px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 24px;
+  width: 100%;
+`;
+
+export const CartCheckOutButton = styled(RectButton)`
+  height: 50px;
+  width: 150px;
+  background-color: #4bd865;
+  border-radius: 50px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CartCheckOutButtonText = styled.Text`
+  color: #f5f5f5;
+  font-weight: bold;
+`;
+
+export const CartFooterValues = styled.View`
+  min-width: 100px;
+  align-items: flex-end;
+`;
+
+export const CartTotalValue = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+`;
+export const CartSubTotalValue = styled.Text`
+  font-size: 18px;
+`;
+export const CartShipValue = styled.Text`
+  font-size: 14px;
+`;
