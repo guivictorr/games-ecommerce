99d2848a200d3c93a3752a66157b778fb570a872
guivictorr <guilhermevictor20@gmail.com>
2021-02-01 18:18:16 -0300

    feat(theme): added theme provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
 import AppProvider from './context';
 import Routes from './routes';
 
-const Main = () => {
-  return (
-    <AppProvider>
-      <Routes />
-    </AppProvider>
-  );
+const theme = {
+  colors: {
+    background: '#0d0d0d',
+    primary: '#ff6523',
+    contrastText: '#e5e5e5',
+    success: '#3affac',
+    error: '#ff3a5e',
+  },
+  borderRadius: '6px',
 };
+
+const Main = () => (
+  <AppProvider>
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  </AppProvider>
+);
 
 export default Main;
